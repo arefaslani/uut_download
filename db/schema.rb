f00d6240abef929e2ea95fefdb5d6d328e9e9c9b
Aref Aslani <arefaslani@gmail.com>
2014-08-08 11:37:46 +0430

    add generate_auth_token to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807102113) do
+ActiveRecord::Schema.define(version: 20140808070053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20140807102113) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "auth_token"
   end
 
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["student_number"], name: "index_users_on_student_number", unique: true, using: :btree
 
