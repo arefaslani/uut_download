6472c515d6db0699f9abf1cd722ddeb7eacf183f
Aref Aslani <arefaslani@gmail.com>
2014-08-08 12:39:05 +0430

    replace old redirects with rails route helpers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     @user = User.find_by!(student_number: session_params[:student_number])
     if @user && @user.authenticate(session_params[:password])
       sign_in @user
-      redirect_to controller: 'users', action: 'show', student_number: @user.student_number
+      redirect_to user_path(@user, student_number: @user.student_number)
     else
       flash.now[:danger] = "There is no such user."
       render :new
