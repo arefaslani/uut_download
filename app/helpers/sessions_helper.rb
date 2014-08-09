affdbcbc697cfed4ee31a3f8d55fd515e11e4489
Aref Aslani <arefaslani@gmail.com>
2014-08-09 23:55:03 +0430

    refactor sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,13 +2,23 @@ module SessionsHelper
   def sign_in(user)
     @user.regenerate_auth_token
     cookies[:auth_token] = @user.auth_token
+    self.current_user = user
   end
 
   def signed_in?
-    User.find_by(auth_token: cookies[:auth_token].to_s)
+    !self.current_user.nil? 
   end
 
   def sign_out
     cookies.delete(:auth_token)
+    self.current_user = nil
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(auth_token: cookies[:auth_token])
   end
 end
