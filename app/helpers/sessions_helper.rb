047fae8b3d233b78cda7debc0c2a226c0074a83c
Aref Aslani <arefaslani@gmail.com>
2014-08-09 22:24:12 +0430

    check whether uesr is signed in or not

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,10 @@ module SessionsHelper
     cookies[:auth_token] = @user.auth_token
   end
 
+  def signed_in?
+    User.find_by(auth_token: cookies[:auth_token].to_s)
+  end
+
   def sign_out
     cookies.delete(:auth_token)
   end
