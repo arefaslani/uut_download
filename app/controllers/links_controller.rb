714ef4e3a3a691960ec15ccb4a11a3e126f4c01f
Aref Aslani <arefaslani@gmail.com>
2014-08-28 14:29:27 +0430

    add links_controller#create to save links to database

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,4 +4,21 @@ class LinksController < ApplicationController
   def new
     @link = current_user.links.new
   end
+
+  def create
+    link = current_user.links.new(link_params)
+    begin
+      link.save!
+      redirect_to current_user
+    rescue
+      flash[:danger] = "Error in saving link!"
+      redirect_to current_user
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:protocol, :address)
+  end
 end
